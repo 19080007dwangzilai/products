d9d40f200d7fcb3a0fef60ce9fba508e83972f1a
wangzilai <19080007d@connect.polyu.hk>
2019-12-14 20:35:26 +0800

    def main and use global variation

--- products.py
+++ products.py
@@ -1,50 +1,55 @@
-# 检查档案的有无
 import os
 
 
+# 读取档案
 def read_file(filename):
-    products = []
-    if os.path.isfile(filename):
-        print('find it')
-        # 读取档案
-        with open(filename, 'r', encoding='utf-8')as f:
-            for line in f:
-                if '商品,价格' in line:
-                    continue
-                name, price = line.strip().split(',')
-                products.append([name, price])
-
-    else:
-        print('not find the file')
-    return products
+    elctronic_products = []
+    with open(filename, 'r', encoding='utf-8')as f:
+        for line in f:
+            if '商品,价格' in line:
+                continue
+            name, price = line.strip().split(',')
+            elctronic_products.append([name, price])
+    return elctronic_products
 
 
-def print_file(products):
-    for p in products:
+def print_file(elctronic_products):
+    for p in elctronic_products:
         print(p[0], '的价格是', p[1])
 
 
-def user_input(products):
+def user_input(elctronic_products):
     # 补充档案
     while True:
         name = input('enter the name of the product:')
         if name == 'q':
             break
         price = input('enter the price of the product:')
-        products.append([name.strip(), price.strip()])
-    print(products)
-    return products
+        elctronic_products.append([name.strip(), price.strip()])
+    print(elctronic_products)
+    return elctronic_products
 
 
 def write_file(filename):
     # 写档案
     with open(filename, 'w', encoding='utf-8')as f:
         f.write('商品,价格\n')
-        for p in products:
+        for p in elctronic_products:
             f.write(p[0] + ',' + p[1] + '\n')
 
 
-products = read_file('products.csv')
-print_file(products)
-products = user_input(products)
-write_file('products.csv')
+def main():
+    filename = 'products.csv'
+    # 全局变量
+    global elctronic_products
+    if os.path.isfile(filename):
+        print('find it')
+        elctronic_products = read_file(filename)
+    else:
+        print('not find the file')
+    print_file(elctronic_products)
+    elctronic_products = user_input(elctronic_products)
+    write_file(filename)
+
+
+main()
